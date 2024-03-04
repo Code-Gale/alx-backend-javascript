27cf0ec604891223df44019057e3a0a7000ddd50
Code-Gale <codewhiz001@gmail.com>
2024-03-04 22:49:12 +0100

    block scoped

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
-    // eslint-disable-line no-unused-vars
-    task = true;
-    // eslint-disable-line no-unused-vars
-    task2 = false;
+    // eslint-disable-next-line no-unused-vars
+    const task = true;
+    // eslint-disable-next-line no-unused-vars
+    const task2 = false;
   }
 
   return [task, task2];
