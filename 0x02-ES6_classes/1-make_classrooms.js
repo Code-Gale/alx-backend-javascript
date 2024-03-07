2b8c06d6b85c00c1020d10850f44aa9da60d104d
Code-Gale <codewhiz001@gmail.com>
2024-03-07 18:36:08 +0100

    make classrooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,6 +2,6 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   // eslint-disable-next-line no-unused-vars
-  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34),];
-  return(rooms)
+  const rooms = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return (rooms);
 }
